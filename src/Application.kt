57cfe54b0a5c38eb705b9cbbeb63d34ef54db9df
Mark Roth <mzroth@gmail.com>
2020-01-20 13:45:48 -0700

    so basic error handling

--- src/Application.kt
+++ src/Application.kt
@@ -9,6 +9,8 @@ import com.fasterxml.jackson.databind.*
 import io.ktor.jackson.*
 import io.ktor.features.*
 
+val BASE_QUERY = "https://api.themoviedb.org/3/search/movie?api_key=944cbf5f14beff1c6148c442b1f2d77c&include_adult=false&query="
+
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
@@ -21,12 +23,10 @@ fun Application.module(testing: Boolean = false) {
     }
 
     routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Html)
-        }
-
-        get("/json/jackson") {
-            call.respond(mapOf("hello" to "world"))
+        get("/movies") {
+            call.request.queryParameters["search"]?.let {
+                
+            } ?: call.respondText("Please provide a search query parameter", status = HttpStatusCode.BadRequest)
         }
     }
 }
