c5f65d0882f6997a202887d35cfb349bf90f7821
Mark Roth <mzroth@gmail.com>
2020-01-20 14:16:35 -0700

    making outside webrequest

--- src/Application.kt
+++ src/Application.kt
@@ -2,12 +2,15 @@ package com.mzroth
 
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
 import io.ktor.http.*
 import com.fasterxml.jackson.databind.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.request.get
 import io.ktor.jackson.*
 import io.ktor.features.*
+import kotlinx.coroutines.withTimeout
 
 val BASE_QUERY = "https://api.themoviedb.org/3/search/movie?api_key=944cbf5f14beff1c6148c442b1f2d77c&include_adult=false&query="
 
@@ -25,8 +28,17 @@ fun Application.module(testing: Boolean = false) {
     routing {
         get("/movies") {
             call.request.queryParameters["search"]?.let {
-                
-            } ?: call.respondText("Please provide a search query parameter", status = HttpStatusCode.BadRequest)
+                val client = HttpClient(OkHttp)
+                try {
+                    withTimeout(5000) {
+                        val response: String = client.get(BASE_QUERY + it)
+                        client.close()
+                        call.respond(HttpStatusCode.OK, response)
+                    }
+                } catch (timeout: Throwable) {
+                    call.respond(HttpStatusCode.InternalServerError, mapOf("readable_message" to "Internal processes timed out"))
+                }
+            } ?: call.respond(HttpStatusCode.BadRequest, mapOf("readable_message" to "Please provide a search query parameter"))
         }
     }
 }
