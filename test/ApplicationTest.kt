8bdf4ab3bdd8448877f8c854a39b3372600b8783
Mark Roth <mzroth@gmail.com>
2020-01-20 16:40:02 -0700

    fixed first test

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -6,6 +6,8 @@ import io.ktor.request.*
 import io.ktor.routing.*
 import io.ktor.http.*
 import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.ktor.jackson.*
 import io.ktor.features.*
 import kotlin.test.*
@@ -15,9 +17,10 @@ class ApplicationTest {
     @Test
     fun testRoot() {
         withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Get, "/search").apply {
+            handleRequest(HttpMethod.Get, "/movies").apply {
                 assertEquals(HttpStatusCode.BadRequest, response.status())
-                assertEquals("HELLO WORLD!", response.content)
+                val readableError: ReadableError = jacksonObjectMapper().readValue(response.content ?: "")
+                assertEquals("Please provide a search query parameter", readableError.readable_message)
             }
         }
     }
---
+++ test/ReadableError.kt
@@ -0,0 +1,5 @@
+package com.mzroth
+
+data class ReadableError(
+    val readable_message: String?
+)
