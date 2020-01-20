83a0e233817e47c426875f7aa34ac1880cf6323d
Mark Roth <mzroth@gmail.com>
2020-01-20 16:42:17 -0700

    made vals public

--- src/Movie.kt
+++ src/Movie.kt
@@ -1,8 +1,8 @@
 package com.mzroth
 
 data class Movie(
-    private val movie_id: Int,
-    private val title: String,
-    private val popularity_summary: String,
-    private val poster_image_url: String? = null
+    val movie_id: Int,
+    val title: String,
+    val popularity_summary: String,
+    val poster_image_url: String? = null
 )
